7362e6ff1a38353082a74d96300aae15ec9d4ce8
Muhammad-Junaid23 <mj23.muhammadjunaid@gmail.com>
2025-08-03 15:38:35 +0500

    sidebar and clerk auth implemented

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,9 @@
-import { UserButton } from '@clerk/nextjs';
 import React from 'react';
 
 const Home = () => {
   return (
     <div>
       <h1>This is a heading in imaginify</h1>
-      <UserButton afterSignOutUrl='/' />
     </div>
   );
 };
